6a51ed80cbd360ddbc9395c60990198528ac7701
codetot-dai <laidai9966@gmail.com>
2023-04-02 22:49:46 +0700

    fix build

--- src/pages/api/dictionary/index.js
+++ src/pages/api/dictionary/index.js
@@ -1,64 +1,64 @@
-import { firestore, fieldValue } from '@/configs/firebase-admin';
+// import { firestore, fieldValue } from '@/configs/firebase-admin';
 
-export default async (req, res) => {
-    if (req.method !== 'POST') res.status(400).end()
+// export default async (req, res) => {
+//     if (req.method !== 'POST') res.status(400).end()
 
-    try {
-        const { word, search, limit = 20, ...param } = req.body;
+//     try {
+//         const { word, search, limit = 20, ...param } = req.body;
 
-        if (search) {
-            const snapshot = await firestore
-                .collection(search[0])
-                .where('word', '>=', search)
-                .where('word', "<=", search + "\uf8ff")
-                .limit(Number(limit))
-                .get()
+//         if (search) {
+//             const snapshot = await firestore
+//                 .collection(search[0])
+//                 .where('word', '>=', search)
+//                 .where('word', "<=", search + "\uf8ff")
+//                 .limit(Number(limit))
+//                 .get()
 
-            const data = snapshot.docs.map(doc => ({
-                id: doc.id,
-                ...doc.data()
-            }));
+//             const data = snapshot.docs.map(doc => ({
+//                 id: doc.id,
+//                 ...doc.data()
+//             }));
 
-            if (data.length) {
-                res.status(200).json({
-                    status: true,
-                    data,
-                    message: 'Get data successfully!',
-                });
-            } else {
-                res.status(200).json({
-                    status: false,
-                    message: 'Not found!',
-                });
-            }
+//             if (data.length) {
+//                 res.status(200).json({
+//                     status: true,
+//                     data,
+//                     message: 'Get data successfully!',
+//                 });
+//             } else {
+//                 res.status(200).json({
+//                     status: false,
+//                     message: 'Not found!',
+//                 });
+//             }
 
-        } else {
-            const alphabetRef = firestore
-                .collection(`${word[0].toLowerCase()}`)
+//         } else {
+//             const alphabetRef = firestore
+//                 .collection(`${word[0].toLowerCase()}`)
 
-            const snapshot = await alphabetRef.where('word', '==', word).get();
+//             const snapshot = await alphabetRef.where('word', '==', word).get();
 
-            if (snapshot.empty) {
-                const { id } = await alphabetRef.add({
-                    word,
-                    timestamp: fieldValue.serverTimestamp(),
-                    ...param,
-                });
+//             if (snapshot.empty) {
+//                 const { id } = await alphabetRef.add({
+//                     word,
+//                     timestamp: fieldValue.serverTimestamp(),
+//                     ...param,
+//                 });
 
-                res.status(200).json({
-                    data: { id },
-                    status: true,
-                    message: 'Created data successfully!',
-                });
-                return;
-            } else {
-                res.status(404).json({
-                    status: false,
-                    message: 'Document exist!',
-                });
-            }
-        }
-    } catch (e) {
-        res.status(400).end();
-    }
-}
+//                 res.status(200).json({
+//                     data: { id },
+//                     status: true,
+//                     message: 'Created data successfully!',
+//                 });
+//                 return;
+//             } else {
+//                 res.status(404).json({
+//                     status: false,
+//                     message: 'Document exist!',
+//                 });
+//             }
+//         }
+//     } catch (e) {
+//         res.status(400).end();
+//     }
+// }
